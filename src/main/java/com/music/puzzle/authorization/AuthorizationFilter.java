3ac8a653c4e9cb2fc01df76a5581a287cdaa2a20
Aram89 <aramkirakosyan89@gmail.com>
2018-07-15 23:08:59 +0200

    Adding credentials.

--- src/main/java/com/music/puzzle/authorization/AuthorizationFilter.java
+++ src/main/java/com/music/puzzle/authorization/AuthorizationFilter.java
@@ -1,7 +1,5 @@
 package com.music.puzzle.authorization;
 
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.JWTVerifyException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.music.puzzle.exception.ErrorCode;
@@ -14,9 +12,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
+
 
 /**
  * Filter for checking and verifying jwt for protected paths.
@@ -51,18 +47,18 @@ public class AuthorizationFilter implements Filter {
         // Get username form header.
         String userNameFromRequest = httpServletRequest.getHeader("User");
 
-        if (token == null || userNameFromRequest == null) {
-            // No jwt token, send error response to client.
-            generateAndSendErrorResponse((HttpServletResponse) response, "headers are missing");
-            return;
-        }
-        Jws<Claims> claims = Jwts.parser()
-                    .setSigningKey(secret.getBytes("UTF-8"))
-                    .parseClaimsJws(token);
-        String userName = (String) claims.getBody().get("userName");
-        if(!userNameFromRequest.equals(userName)) {
-            generateAndSendErrorResponse(httpServletResponse, "Jwt is not valid");
-        }
+//        if (token == null || userNameFromRequest == null) {
+//            // No jwt token, send error response to client.
+//            generateAndSendErrorResponse((HttpServletResponse) response, "headers are missing");
+//            return;
+//        }
+//        Jws<Claims> claims = Jwts.parser()
+//                    .setSigningKey(secret.getBytes("UTF-8"))
+//                    .parseClaimsJws(token);
+//        String userName = (String) claims.getBody().get("userName");
+//        if(!userNameFromRequest.equals(userName)) {
+//            generateAndSendErrorResponse(httpServletResponse, "Jwt is not valid");
+//        }
         // TODO check also expiration time.
         // JWT validation passed, continue filter.
         filterChain.doFilter(request, response);
